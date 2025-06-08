1eee9094fc688d730a01246ae89e4f8feafe97f1
JDR89 <joaquin.romero89@gmail.com>
2025-06-08 10:52:37 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from autenticacion import Autenticacion
+from menu import Menu
+
+def main():
+    sistema = Autenticacion()
+    menu = Menu()
+
+    while True:
+        print("\n=== SISTEMA DE USUARIOS ===")
+        print("1. Registrarse")
+        print("2. Iniciar sesión")
+        print("0. Salir")
+        opcion = input("Elegí una opción: ")
+
+        if opcion == "1":
+            sistema.registrar_usuario()
+        elif opcion == "2":
+            usuario = sistema.iniciar_sesion()
+            if usuario:
+                if usuario.rol == "admin":
+                    menu.mostrar_menu_admin(usuario, sistema.usuarios)
+                else:
+                    menu.mostrar_menu_usuario(usuario)
+        elif opcion == "0":
+            break
+        else:
+            print("❌ Opción inválida.")
+
+if __name__ == "__main__":
+    main()
