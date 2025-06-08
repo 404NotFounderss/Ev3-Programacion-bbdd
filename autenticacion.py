1c7b03a677558e391496196c281b91061198148f
IL-TOPO <ganatale86@gmail.com>
2025-06-08 19:29:46 -0300

    se agregan ficheros autenticacion.py y menu.py feature/natale-gabriel

---
+++ autenticacion.py
@@ -0,0 +1,58 @@
+from usuario import Usuario
+
+class Autenticacion:
+    def __init__(self):
+        self.usuarios = []
+        self.next_id = 1
+
+        admin_prueba = Usuario(
+            id_usuario=self.next_id,
+            nombre="Admin",
+            email="admin@admin.com",
+            contrasena="asd123",
+            rol="admin"
+        )
+        usuario_prueba = Usuario(
+            id_usuario=self.next_id + 1,
+            nombre="usuario_prueba",
+            email="usuario@usuario.com",
+            contrasena="asd123",
+            rol="usuario"
+        )
+
+        self.usuarios.append(admin_prueba)
+        self.next_id += 1
+        self.usuarios.append(usuario_prueba)
+        self.next_id += 1
+
+    def registrar_usuario(self):
+        nombre = input("Nombre: ")
+        email = input("Email: ")
+        contrasena = input("Contraseña (mínimo 6 caracteres, letras y números): ")
+        rol = "usuario"
+
+        if len(contrasena) < 6 or contrasena.isalpha() or contrasena.isdigit():
+            print("La contraseña debe tener al menos 6 caracteres y contener letras y números.")
+            return
+
+        for u in self.usuarios:
+            if u.email == email:
+                print("❌ Ya existe un usuario con ese email.")
+                return
+
+        usuario = Usuario(self.next_id, nombre, email, contrasena,rol)
+        self.usuarios.append(usuario)
+        self.next_id += 1
+        print("✅ Usuario registrado con éxito.")
+
+    def iniciar_sesion(self):
+        email = input("Email: ")
+        contrasena = input("Contraseña: ")
+
+        for usuario in self.usuarios:
+            if usuario.email == email and usuario.contrasena == contrasena:
+                print(f"🔓 Bienvenido, {usuario.nombre}!")
+                return usuario
+
+        print("❌ Credenciales incorrectas.")
+        return None
---
+++ menu.py
@@ -0,0 +1,47 @@
+from gestor_usuarios import GestorUsuarios
+
+class Menu:
+    def mostrar_menu_usuario(self, usuario):
+        while True:
+            print("\n--- MENÚ USUARIO ---")
+            print("1. Ver mis datos")
+            print("2. Cambiar contraseña")
+            print("0. Salir")
+
+            opcion = input("Elegí una opción: ")
+
+            if opcion == "1":
+                usuario.ver_datos()
+            elif opcion == "2":
+                nueva = input("Nueva contraseña: ")
+                usuario.cambiar_contrasena(nueva)
+            elif opcion == "0":
+                break
+            else:
+                print("❌ Opción inválida.")
+
+    def mostrar_menu_admin(self, usuario, usuarios):
+        gestor = GestorUsuarios(usuarios)
+
+        while True:
+            print("\n--- MENÚ ADMIN ---")
+            print("1. Listar usuarios")
+            print("2. Eliminar usuario")
+            print("3. Cambiar rol")
+            print("0. Salir")
+
+            opcion = input("Elegí una opción: ")
+
+            if opcion == "1":
+                gestor.listar_usuarios()
+            elif opcion == "2":
+                id_usuario = int(input("ID del usuario a eliminar: "))
+                gestor.eliminar_usuario(id_usuario)
+            elif opcion == "3":
+                id_usuario = int(input("ID del usuario: "))
+                nuevo_rol = input("Nuevo rol (admin/usuario): ")
+                gestor.cambiar_rol(id_usuario, nuevo_rol)
+            elif opcion == "0":
+                break
+            else:
+                print("❌ Opción inválida.")
